16d6906853d50ac2d8cd0293f2840c725ebddd38
ef-ext-arielcanete <ext.ariel.canete@ef.com>
2024-05-29 13:05:33 +0800

    saving changes

--- src/app/components/LocationGallery.jsx
+++ src/app/components/LocationGallery.jsx
@@ -551,7 +551,7 @@ export default function LocationGallery({ onClose, name, images }) {
                     cols={4}
                     rowHeight={401}
                   >
-                  {currentImages.map((image, index) => (
+                  {currentImages.map((image) => (
                     <ImageListItem 
                       key={image.img}
                       cols={image.cols || 1} rows={image.rows || 1}
