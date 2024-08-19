1f896566621894b295c84e59ad0d06a70c336fa1
HKCreativeStudio <jennifer.chua@ef.com>
2024-08-20 01:04:05 +0800

    fixed mobile photo upload height

--- src/app/components/PhotoUpload.jsx
+++ src/app/components/PhotoUpload.jsx
@@ -134,7 +134,7 @@ function PhotoSubmitForm({ isOpen, handleClose, setIsThankYouOpen }) {
         <Fade in={isOpen}>
           <div style={{
             // position: 'absolute',
-            height: '190vh',
+            height: '140vh',
             width: '100vw',
             overflow: 'scroll',
             display: 'flex',
