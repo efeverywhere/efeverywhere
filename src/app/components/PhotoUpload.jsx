0abb8568b857f12d4296ba0b3126002ac1e980a5
HKCreativeStudio <jennifer.chua@ef.com>
2024-08-20 11:28:52 +0800

    again

--- src/app/components/PhotoUpload.jsx
+++ src/app/components/PhotoUpload.jsx
@@ -134,7 +134,7 @@ function PhotoSubmitForm({ isOpen, handleClose, setIsThankYouOpen }) {
         <Fade in={isOpen}>
           <div style={{
             // position: 'absolute',
-            height: isEFStaff == 'Yes' | isEFStudent == 'Yes' ? '170vh' : '140vh',
+            height: isEFStaff == 'Yes' | isEFStudent == 'Yes' ? '170vh' : '150vh',
             width: '100vw',
             overflow: 'scroll',
             display: 'flex',
