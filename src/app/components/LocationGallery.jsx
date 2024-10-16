2fc48d2f7cffa93ff1711ebeb633f1abf30f7b00
HKCreativeStudio <jennifer.chua@ef.com>
2024-10-16 13:53:19 +0000

    Test

--- src/app/components/LocationGallery.jsx
+++ src/app/components/LocationGallery.jsx
@@ -502,7 +502,7 @@ export default function LocationGallery({ onClose, name, images, countryISOCode,
                       fontSize: '20px'
                     }}
                   >
-                    {name}
+                    {name}, {country}
                   </Typography>
                 </div>
                 {images && viewState === 'squares' &&
@@ -1241,7 +1241,7 @@ export default function LocationGallery({ onClose, name, images, countryISOCode,
                           fontSize: '64px'
                         }}
                       >
-                        {name}
+                        {name}, {country}
                       </Typography>
                     </div>
                     <div
